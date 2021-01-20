7bff57a4b40106a6394268f0e292b585f4289b79
laotang123 <duo199702180106>
2021-01-21 07:51:11 +0800

    修复bug：通配符jar文件匹配

--- src/main/java/org/ljf/sjvm/book/ch02/classpath/WildcardEntry.java
+++ src/main/java/org/ljf/sjvm/book/ch02/classpath/WildcardEntry.java
@@ -1,6 +1,8 @@
 package org.ljf.sjvm.book.ch02.classpath;
 
 import org.ljf.sjvm.book.util.IOUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.Arrays;
@@ -13,34 +15,49 @@ import java.util.Arrays;
  * @version: $ 1.0
  */
 public class WildcardEntry implements Entry {
+    private static final Logger logger = LoggerFactory.getLogger(WildcardEntry.class);
+
+    private static final String CLASS = "class";
+    private static final String JAR = "jar";
     private final Entry compositeEntry;
 
     public WildcardEntry(String contextPath) {
-        String baseDir = contextPath.substring(0,contextPath.length() - 2);
-//        String filePaths = walkFunc(baseDir);
-        this.compositeEntry = EntryFactory.newEntry(baseDir);
+        String baseDir = contextPath.substring(0, contextPath.length() - 2);
+        String filePaths = walkFunc(baseDir);
+        this.compositeEntry = EntryFactory.newEntry(filePaths);
     }
 
-//    public String walkFunc(String walkPath) {
-//        File walkFile = new File(walkPath);
-//
-//        StringBuilder stringBuilder = new StringBuilder();
-//        File[] listFiles = walkFile.listFiles();
-//        if (listFiles != null) {
-//            for (File file : listFiles) {
-//                if (file.isFile()) {
-//                    stringBuilder.append(file.getParent()).append(File.pathSeparator);
-//                }
-//            }
-//        }
-//
-//        //空路径填充为当前工作路径
-//        String filePaths = stringBuilder.toString();
-//        if (filePaths.isEmpty()){
-//            filePaths = IOUtil.abs(walkPath);
-//        }
-//        return filePaths;
-//    }
+    public String walkFunc(String walkPath) {
+        File walkFile = new File(walkPath);
+        boolean hasParentPath = false;//通配符匹配到class文件时将当前工作路径加入
+
+        StringBuilder stringBuilder = new StringBuilder();
+        File[] listFiles = walkFile.listFiles();
+        String suffix;
+        if (listFiles != null) {
+            for (File file : listFiles) {
+                if (file.isFile()) {
+                    suffix = IOUtil.getSuffix(file);
+                    if (!hasParentPath && suffix.equals(CLASS)) {//class文件只添加一次当前工作路径
+                        stringBuilder.append(file.getParent()).append(File.pathSeparator);
+                        hasParentPath = true;
+                    } else if (suffix.equalsIgnoreCase(JAR)) {//jar文件添加全路径
+                        stringBuilder.append(file.getPath()).append(File.pathSeparator);
+                    }
+                    logger.info("wildcard entry add file: " + file.getPath());
+                }
+            }
+        }
+
+        //空路径填充为当前工作路径
+        String filePaths = stringBuilder.toString();
+        if (filePaths.isEmpty()) {
+            filePaths = IOUtil.abs(walkPath);
+        } else {
+            filePaths = filePaths.substring(0, filePaths.length() - 1);//去除最后一个';'
+        }
+        return filePaths;
+    }
 
     @Override
     public byte[] readClass(String className) {
