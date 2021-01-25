700abe7cf43be46785e2a7b5e634979101588c5c
liujunfeng <liujunfeng@golaxy.cn>
2021-01-25 17:05:52 +0800

    已实现功能

---
+++ src/main/java/org/ljf/sjvm/classfile/SourceFileAttribute.java
@@ -0,0 +1,32 @@
+package org.ljf.sjvm.classfile;
+
+import org.ljf.sjvm.classfile.constantpool.ConstantPool;
+
+/**
+ * @author: ljf
+ * @date: 2021/1/25 16:07
+ * @description: SourceFile_attribute {
+ * u2 attribute_name_index;
+ * u4 attribute_length;
+ * u2 sourcefile_index;
+ * }
+ * @modified By：
+ * @version: $ 1.0
+ */
+public class SourceFileAttribute implements AttributeInfo {
+    private int sourceFileIndex;
+    private final ConstantPool constantPool;
+
+    public SourceFileAttribute(ConstantPool constantPool) {
+        this.constantPool = constantPool;
+    }
+
+    @Override
+    public void readInfo(ClassReader reader) {
+        this.sourceFileIndex = reader.readUint16();
+    }
+
+    public String getSourceFileName() {
+        return this.constantPool.getUtf8(sourceFileIndex);
+    }
+}
---
+++ src/main/java/org/ljf/sjvm/classfile/UnparsedAttribute.java
@@ -0,0 +1,32 @@
+package org.ljf.sjvm.classfile;
+
+/**
+ * @author: ljf
+ * @date: 2021/1/25 16:09
+ * @description: java虚拟机不识别属性
+ * @modified By：
+ * @version: $ 1.0
+ */
+public class UnparsedAttribute implements AttributeInfo {
+    private final String name;
+    private final long length;//uint32
+    private byte[] info;
+
+    public UnparsedAttribute(String attrName, long attrLen) {
+        this.name = attrName;
+        this.length = attrLen;
+    }
+
+    @Override
+    public void readInfo(ClassReader reader) {
+        this.info = reader.readBytes(this.length);
+    }
+
+    public byte[] getInfo() {
+        return this.info;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
