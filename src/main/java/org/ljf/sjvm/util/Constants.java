6de369f242eef4e90e9af391472310c19d4f6dff
liujunfeng <liujunfeng@golaxy.cn>
2021-01-26 14:05:51 +0800

    添加魔数对应的版本常量值

---
+++ src/main/java/org/ljf/sjvm/util/Constants.java
@@ -0,0 +1,15 @@
+package org.ljf.sjvm.util;
+
+/**
+ * @author: ljf
+ * @date: 2021/1/26 14:01
+ * @description: 常量值
+ * @modified By：
+ * @version: $ 1.0
+ */
+public interface Constants {
+    int JDK1_OR_JDK1_1 = 45;
+    int JDK1_2 = 46;
+    int JDK8 = 52;
+    int ZERO = 0;
+}
