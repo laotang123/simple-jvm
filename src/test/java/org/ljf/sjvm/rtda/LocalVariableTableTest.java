12eaac4e9b8df7b253f294046e044f1014ec9cda
laotang123 <duo199702180106>
2021-01-28 07:48:13 +0800

    单元测试栈帧局部变量表和操作数表

---
+++ src/test/java/org/ljf/sjvm/rtda/LocalVariableTableTest.java
@@ -0,0 +1,37 @@
+package org.ljf.sjvm.rtda;
+
+/**
+ * @author: ljf
+ * @date: 2021/1/28 7:29
+ * @description: 本地方法表测试
+ * 栈帧的元素之一，包含slots数组
+ * @modified By:
+ * @version: $ 1.0
+ */
+public class LocalVariableTableTest {
+    public static void main(String[] args) {
+        LocalVariableTable table = new LocalVariableTable(20);
+        table.setInt(0,Integer.MAX_VALUE);
+        System.out.println(table.getInt(0) == Integer.MAX_VALUE);
+        table.setInt(1,Integer.MIN_VALUE);
+        System.out.println(table.getInt(1) == Integer.MIN_VALUE);
+
+        //long，占两个slot
+        table.setLong(2,Long.MAX_VALUE);
+        System.out.println(table.getLong(2) == Long.MAX_VALUE);
+        table.setLong(4,Long.MIN_VALUE);
+        System.out.println(table.getLong(4) == Long.MIN_VALUE);
+
+        //float
+        table.setFloat(6,Float.MAX_VALUE);
+        System.out.println(table.getFloat(6) == Float.MAX_VALUE);
+        table.setFloat(7,Float.MIN_VALUE);
+        System.out.println(table.getFloat(7) == Float.MIN_VALUE);
+
+        //double，占两个slot
+        table.setDouble(8,Double.MAX_VALUE);
+        System.out.println(table.getDouble(8) == Double.MAX_VALUE);
+        table.setDouble(10,Double.MIN_VALUE);
+        System.out.println(table.getDouble(10) == Double.MIN_VALUE);
+    }
+}
---
+++ src/test/java/org/ljf/sjvm/rtda/OperandStackTest.java
@@ -0,0 +1,38 @@
+package org.ljf.sjvm.rtda;
+
+/**
+ * @author: ljf
+ * @date: 2021/1/28 7:30
+ * @description: 操作数栈测试
+ * @modified By:
+ * @version: $ 1.0
+ */
+public class OperandStackTest {
+    public static void main(String[] args) {
+        OperandStack operandStack = new OperandStack(10);
+
+        //int
+        operandStack.pushInt(Integer.MAX_VALUE);
+        System.out.println(operandStack.popInt() == Integer.MAX_VALUE);
+        operandStack.pushInt(Integer.MIN_VALUE);
+        System.out.println(operandStack.popInt() == Integer.MIN_VALUE);
+
+        //long
+        operandStack.pushLong(Long.MAX_VALUE);
+        System.out.println(operandStack.popLong() == Long.MAX_VALUE);
+        operandStack.pushLong(Long.MIN_VALUE);
+        System.out.println(operandStack.popLong() == Long.MIN_VALUE);
+
+        //float
+        operandStack.pushFloat(Float.MAX_VALUE);
+        System.out.println(operandStack.popFloat() == Float.MAX_VALUE);
+        operandStack.pushFloat(Float.MIN_VALUE);
+        System.out.println(operandStack.popFloat() == Float.MIN_VALUE);
+
+        //double
+        operandStack.pushDouble(Double.MAX_VALUE);
+        System.out.println(operandStack.popDouble() == Double.MAX_VALUE);
+        operandStack.pushDouble(Double.MIN_VALUE);
+        System.out.println(operandStack.popDouble() == Double.MIN_VALUE);
+    }
+}
