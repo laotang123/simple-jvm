ea27d0cf446334a44987cccdc4148a1c7b01dc99
laotang123 <duo199702180106>
2021-01-30 11:55:13 +0800

    布尔指令

---
+++ src/main/java/org/ljf/sjvm/instructions/math/IXor.java
@@ -0,0 +1,20 @@
+package org.ljf.sjvm.instructions.math;
+
+import org.ljf.sjvm.instructions.base.NoOperandInstruction;
+import org.ljf.sjvm.rtda.Frame;
+
+/**
+ * @author: ljf
+ * @date: 2021/1/30 11:49
+ * @description: int 类型的异或操作
+ * @modified By:
+ * @version: $ 1.0
+ */
+public class IXor extends NoOperandInstruction {
+    @Override
+    public void execute(Frame frame) {
+        int v2 = frame.popInt();
+        int v1 = frame.popInt();
+        frame.pushInt(v2 ^ v1);
+    }
+}
