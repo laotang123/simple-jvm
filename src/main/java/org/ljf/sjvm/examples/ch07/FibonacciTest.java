875d07b016625ff982220680a189859e50d3dfe6
liujunfeng <liujunfeng@golaxy.cn>
2021-02-21 10:48:28 +0800

    静态模块，包含clinit方法

--- src/main/java/org/ljf/sjvm/examples/ch07/FibonacciTest.java
+++ src/main/java/org/ljf/sjvm/examples/ch07/FibonacciTest.java
@@ -4,10 +4,14 @@ package org.ljf.sjvm.examples.ch07;
  * @author: ljf
  * @date: 2021/2/8 6:54
  * @description: 斐波那契数列计算
+ * 含有静态模块或静态属性，有clinit类出初始化方法
  * @modified By:
  * @version: $ 1.0
  */
 public class FibonacciTest {
+    static {
+        //for clinit method
+    }
     public static void main(String[] args) {
         long x = fibonacci(30);
         System.out.println(x);
