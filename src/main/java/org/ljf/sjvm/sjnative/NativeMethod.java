17772a19fd07576386be7838a72059522fdd7744
laotang123 <duo199702180106>
2021-02-26 08:14:13 +0800

    本地方法注册部分实现

---
+++ src/main/java/org/ljf/sjvm/sjnative/NativeMethod.java
@@ -0,0 +1,14 @@
+package org.ljf.sjvm.sjnative;
+
+import org.ljf.sjvm.classfile.MemberInfo;
+import org.ljf.sjvm.rtda.heap.Method;
+
+/**
+ * @author: ljf
+ * @date: 2021/2/26 8:13
+ * @description: 本地方法
+ * @modified By:
+ * @version: $ 1.0
+ */
+public class NativeMethod extends Method {
+}
---
+++ src/main/java/org/ljf/sjvm/sjnative/Registry.java
@@ -0,0 +1,16 @@
+package org.ljf.sjvm.sjnative;
+
+import org.ljf.sjvm.rtda.Frame;
+
+import java.util.Map;
+
+/**
+ * @author: ljf
+ * @date: 2021/2/26 8:08
+ * @description: 本地方法注册和查找
+ * @modified By:
+ * @version: $ 1.0
+ */
+public class Registry {
+    private Map<String, NativeMethod> registry;
+}
